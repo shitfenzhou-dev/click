bed431508d587e4c2ace5af2ffc7b95955e384d3
lwj <shitfenzhou@gmail.com>
2026-05-28 09:29:09 +0000

    lwj

--- src/click/types.py
+++ src/click/types.py
@@ -423,8 +423,8 @@ class Choice(ParamType[ParamTypeValue], t.Generic[ParamTypeValue]):
         if self.case_sensitive:
             matched = (c for c in str_choices if c.startswith(incomplete))
         else:
-            incomplete = incomplete.lower()
-            matched = (c for c in str_choices if c.lower().startswith(incomplete))
+            incomplete = incomplete.casefold()
+            matched = (c for c in str_choices if c.startswith(incomplete))
 
         return [CompletionItem(c) for c in matched]
 
